5a5407137c3cb7e1e659848e1151dbd86cc3179c
Frederic Heath <F.Heath@posteo.net>
2024-09-19 23:27:51 +0200

    Replaced foot terminal emulator with Alacritty.

--- sway/scripts/close-all.sh
+++ sway/scripts/close-all.sh
@@ -1,8 +1,7 @@
 #!/bin/zsh
 set -e;
 
-if pgrep brave >/dev/null; then echo "Shutting down Brave..." && swaymsg "[class=\"Brave-browser\"] kill"; fi
-if pgrep foot >/dev/null; then echo "Shutting down foot..." && swaymsg "[app_id=\"foot\"] kill"; fi
+if pgrep Alacritty >/dev/null; then echo "Shutting down Alacritty..." && swaymsg "[app_id=\"Alacritty\"] kill"; fi
 if pgrep idea >/dev/null; then echo "Shutting down IntelliJ..." && swaymsg "[class=\"jetbrains-idea-ce\"] kill"; fi
 if pgrep tmux >/dev/null; then echo "Shutting down Tmux..." && swaymsg "[class=\"tmux\"] kill"; fi
 if pgrep firefox >/dev/null; then echo "Shutting down Firefox..." && swaymsg "[class=\"firefox\"]"; fi
--- sway/scripts/swayinitapps.sh
+++ sway/scripts/swayinitapps.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-if ! pgrep foot >/dev/null; then echo "Launching foot..." && swaymsg "exec /usr/bin/foot"; fi
-if [ $(pgrep tmux | wc -l) -lt 1 ]; then echo "Launching tmux..." && swaymsg "exec /usr/bin/foot --app-id tmuxed_foot /usr/bin/tmux"; fi
+if ! pgrep alacritty >/dev/null; then echo "Launching Alacritty..." && swaymsg "exec /usr/bin/alacritty"; fi
+if [ $(pgrep tmux | wc -l) -lt 1 ]; then echo "Launching Tmux..." && swaymsg "exec /usr/bin/alacritty --class tmuxed_crit -e /usr/bin/tmux"; fi
 if ! pgrep firefox >/dev/null; then echo "Launching Firefox..." && swaymsg "exec /usr/bin/firefox"; fi
-if ! pgrep thunderbird >/dev/null; then echo "Launching Thunderbird..." && swaymsg "exec /usr/bin/thunderbird"; fi
