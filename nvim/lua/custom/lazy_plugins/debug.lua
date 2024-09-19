9e3e94d1fb7801d137ab81633fed27339129c8cd
Frederic Heath <F.Heath@posteo.net>
2024-09-19 23:22:01 +0200

    Removed auto setup to prevent wrong java related extension setups. Replaced jdtls with java debug adapter shorthand javadbg. Added javatest shorthand for java test framework support. kotlin shorthand

--- nvim/lua/custom/lazy_plugins/debug.lua
+++ nvim/lua/custom/lazy_plugins/debug.lua
@@ -21,7 +21,7 @@ return {
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
-      automatic_setup = true,
+      -- automatic_setup = true,
 
       -- You can provide additional configuration to the handlers,
       -- see mason-nvim-dap README for more information
@@ -31,10 +31,9 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'jdtls',
-        'kotlin-debug-adaptor',
-
-
+        'javadbg',
+        'javatest',
+        'kotlin',
       },
     }
 
