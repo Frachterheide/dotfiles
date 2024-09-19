3d096b19b605fb2444626b94199cec5eb9eaad00
Frederic Heath <F.Heath@posteo.net>
2024-09-19 23:16:22 +0200

    Added _JAVA_OPTIONS to introduce XDG base dir spec support for java. Added WINIT_UNIX_BACKEND to enable wayland mode for Alacritty.

--- env/interactive.sh
+++ env/interactive.sh
@@ -4,6 +4,7 @@ else
  export EDITOR='nvim'
 fi
 
+export WINIT_UNIX_BACKEND=wayland
 export INPUTRC=${XDG_CONFIG_HOME}/input/config
 export SDIRS=${XDG_DATA_HOME}/sdirs
 export MARKPATH=${XDG_DATA_HOME}/marks
@@ -11,6 +12,7 @@ export MARKPATH=${XDG_DATA_HOME}/marks
 export WGETRC="$XDG_CONFIG_HOME"/wgetrc
 export MAVEN_OPTS=-Dmaven.repo.local="$XDG_DATA_HOME/maven/repository"
 export GRADLE_HOME="$XDG_DATA_HOME"/gradle
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
 export PATH="$PATH:$XDG_DATA_HOME/cargo/bin"
 export RUSTUP_HOME="${XDG_DATA_HOME}"/rustup
 export CARGO_HOME="${XDG_DATA_HOME}"/cargo
