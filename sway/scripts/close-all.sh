70e4fdd21eabb713da7525429cadee04ec611be2
Frederic Heath <F.Heath@posteo.net>
2024-09-22 15:24:08 +0200

    Replaced Alacritty as default terminal emulator.

--- sway/scripts/close-all.sh
+++ sway/scripts/close-all.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 set -e;
 
-if pgrep Alacritty >/dev/null; then echo "Shutting down Alacritty..." && swaymsg "[app_id=\"Alacritty\"] kill"; fi
+if pgrep wezterm >/dev/null; then echo "Shutting down Alacritty..." && swaymsg "[app_id=\"org.wezfurlong.wezterm\"] kill"; fi
 if pgrep idea >/dev/null; then echo "Shutting down IntelliJ..." && swaymsg "[class=\"jetbrains-idea-ce\"] kill"; fi
-if pgrep tmux >/dev/null; then echo "Shutting down Tmux..." && swaymsg "[class=\"tmux\"] kill"; fi
+if pgrep tmux >/dev/null; then echo "Shutting down Tmux..." && swaymsg "[app_id=\"tmuxed_term\"] kill"; fi
 if pgrep firefox >/dev/null; then echo "Shutting down Firefox..." && swaymsg "[class=\"firefox\"]"; fi
 if pgrep spotify >/dev/null; then echo "Shutting down Spotify..." && swaymsg "[class=\"Spotify\"] kill"; fi
 if pgrep thunderbird >/dev/null; then echo "Shutting down Thunderbird..." && swaymsg "[class=\"thunderbird\"] kill"; fi
--- sway/scripts/swayinitapps.sh
+++ sway/scripts/swayinitapps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if ! pgrep alacritty >/dev/null; then echo "Launching Alacritty..." && swaymsg "exec /usr/bin/alacritty"; fi
-if [ $(pgrep tmux | wc -l) -lt 1 ]; then echo "Launching Tmux..." && swaymsg "exec /usr/bin/alacritty --class tmuxed_crit -e /usr/bin/tmux"; fi
+if ! pgrep wezterm >/dev/null; then echo "Launching Wezterm..." && swaymsg "exec /usr/bin/wezterm"; fi
+if [ $(pgrep tmux | wc -l) -lt 1 ]; then echo "Launching Tmux..." && swaymsg "exec /usr/bin/wezterm -e --class tmuxed_term /usr/bin/tmux"; fi
 if ! pgrep firefox >/dev/null; then echo "Launching Firefox..." && swaymsg "exec /usr/bin/firefox"; fi
