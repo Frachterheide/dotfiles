257d449b5610510b0d3bc75ce3d711ab9338b6a6
Frederic Heath <F.Heath@posteo.net>
2025-02-27 15:15:33 +0100

    Added comments. Changed fidget.nvim notification background opacity.

--- nvim/lua/custom/lazy_plugins/lsp.lua
+++ nvim/lua/custom/lazy_plugins/lsp.lua
@@ -4,10 +4,10 @@ return {
         -- Automatically install LSPs to stdpath for neovim
         'williamboman/mason-lspconfig.nvim',
         'neovim/nvim-lspconfig',
-        -- completion 
+        -- completion
         'hrsh7th/cmp-nvim-lsp',
-        -- notifications & LSP nrogress messages 
-        { 'j-hui/fidget.nvim', opts = {} }, -- opts = {} instead of require(x).setup({})
+        -- notifications & LSP nrogress messages
+        { 'j-hui/fidget.nvim', opts = { notification = { window = { winblend = 0, }, }, }, }, -- opts = {} instead of require(x).setup({})
         -- automatic lua-language-server config
         'folke/lazydev.nvim',
     }
