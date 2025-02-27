f37378303b7464bb56594aed949e9105f0682edd
Frederic Heath <F.Heath@posteo.net>
2025-02-27 16:59:46 +0100

    Swapped default/tmuxed alacritty for wezterm. Added LibreWolf as default search browser.

--- sway/scripts/swayinitapps.sh
+++ sway/scripts/swayinitapps.sh
@@ -3,3 +3,4 @@
 if ! pgrep wezterm >/dev/null; then echo "Launching Wezterm..." && swaymsg "exec /usr/bin/wezterm"; fi
 if [ $(pgrep tmux | wc -l) -lt 1 ]; then echo "Launching Tmux..." && swaymsg "exec /usr/bin/wezterm -e --class tmuxed_term /usr/bin/tmux"; fi
 if ! pgrep firefox >/dev/null; then echo "Launching Firefox..." && swaymsg "exec /usr/bin/firefox"; fi
+if ! pgrep librewolf >/dev/null; then echo "Launching Librewolf..." && swaymsg "exec /usr/bin/librewolf"; fi
