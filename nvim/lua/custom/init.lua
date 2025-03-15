2533b9cd88716689c71dc85bda6587d097c7084a
Frederic Heath <F.Heath@posteo.net>
2025-03-15 18:58:25 +0100

    Renamed subdirectories to indicate appropriate content more clearly. Rearranged files accordingly.

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -1,7 +1,3 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
--- See the kickstart.nvim README for more information
-require("custom.init_config")
+require("custom.init_setup")
 require("custom.lazy")
-require("custom.lsp_config")
-require("custom.after_config")
+require("custom.post_lazy")
