71a98b75a3ba76a3fcfcf5a6a97723638980d868
Frederic Heath <F.Heath@posteo.net>
2025-03-21 23:37:20 +0100

    Removed vscode-java-test bundles from jdtls extensions. Removed unnecessary comments and empty lines.

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -8,7 +8,7 @@ local jdtls = require('jdtls')
 local filetypes = { 'java' }
 -- File types that signify a Java project's root directory. This will be
 -- used by eclipse to determine what constitutes a workspace
-local root_markers = { 'gradlew', 'mvnw', '.git', 'pom.xml', 'build.gradle' }
+local root_markers = { 'pom.xml', 'mvnw', '.git', 'gradlew', 'build.gradle' }
 local root_dir = function()
     return jdtls.setup.find_root(root_markers)
 end
@@ -18,10 +18,12 @@ local mason_share = home .. "/.local/share/nvim/mason/share"
 local jdtls_package = mason_packages .. "/jdtls"
 local google_style = mason_share .. "/jdtls/eclipse-java-google-style.xml"
 local jdtls_jar = vim.fn.glob(jdtls_package .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+-- add debug extension
 local bundles = {
-    vim.fn.glob(home .. '/.local/share/nvim/mason/share/java-debug-adapter/com.microsoft.java.debug.plugin.jar', true)
+    vim.fn.glob(home .. mason_share ..'/java-debug-adapter/com.microsoft.java.debug.plugin.jar', true)
 };
-vim.list_extend(bundles, vim.split(vim.fn.glob(home .. '/.local/share/nvim/mason/share/java-test/*.jar', true), '\n'))
+-- add test extension
+-- vim.list_extend(bundles, vim.split(vim.fn.glob(home .. mason_share .. "/java-test/*.jar", true), "\n"))
 local jdtls_config_path = jdtls_package .. "/config_linux"
 local lombok_jar = jdtls_package .. "/lombok.jar"
 -- eclipse.jdt.ls stores project specific data within a folder. If you are working
@@ -148,15 +150,10 @@ local jdtls_config = function(caps)
         root_dir = root_dir(),       -- Set the root directory to our found root_marker
         -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
         init_options = {
-            -- debug adapter
+            -- jdtls extensions
             bundles = bundles
         },
         capabilities = caps,
-        -- cmd is the command that starts the language server. Whatever is placed
-        -- here is what is passed to the command line to execute jdtls.
-        -- Note that eclipse.jdt.ls must be started with a Java version of 21 or higher
-        -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-        -- for the full list of options
         cmd = {
             '/usr/lib/jvm/java-21-openjdk/bin/java',
             '-Declipse.application=org.eclipse.jdt.ls.core.id1',
