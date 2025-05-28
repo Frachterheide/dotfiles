8c43a30b5c52ca2e22ee4252d84b90fa839fa3fa
Frederic Heath <F.Heath@posteo.net>
2025-05-28 15:51:51 +0200

    Improved compliance towards POSIX shells. Removed unnecessary checks for path starting characters. Added username to path information. Renamed tmux/scripts/short_path.bash to tmux/scripts/short_path.sh .

--- tmux/scripts/short_path.sh
+++ tmux/scripts/short_path.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 path=$1
-shortPBase=""
+shortPBase="[$(whoami)]@ "
 shortPath=""
 maxDirs=3
 
-[[ -z "${path}" ]] && echo "" >&1
+[ -z "${path}" ] && echo "" >&1
 #remove leading slash
 path="$(echo "${path}" | sed -r 's/^\///')"
 #create array from directory names
@@ -15,29 +15,27 @@ count=${#path_l[@]}
 unset dir
 unset path
 #replace leading dirs with visual prefix (remove from array)
-if [[ "${path_l[0]}" == "home" ]]; then
+if [ "${path_l[0]}" == "home" ]; then
     shortPBase+='~/'
-    if [[ $count -le $maxDirs ]]; then
+    if [ $count -le $maxDirs ]; then
         echo $shortPBase >&1
         exit 0;
     else
         path_l=("${path_l[@]:2}")
     fi
-elif [[ "${path_l[0]}" == "." ]]; then
-    shortPBase+='./'
 else
     shortPBase+='/'
 fi
 #replace remaining surplus dirs 
 count=${#path_l[@]}
-if [[ $count -lt $maxDirs ]];then
+if [ $count -lt $maxDirs ];then
     maxDirs=$count 
 else
     shortPBase+=".../"
 fi
 #append remaining last maxDirs director
 for dir in $(seq "$maxDirs");do
-    if [[ $dir != "" ]]; then
+    if [ $dir != "" ]; then
         shortPath="${path_l[$((count-dir))]}/${shortPath}"
     fi
 done
