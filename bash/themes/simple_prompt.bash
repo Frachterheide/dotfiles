27e735939d6fbc9b2d9c185c48c440b3ead32044
Frederic Heath <F.Heath@posteo.net>
2025-05-28 15:53:07 +0200

    Added username in front of path. Shortened path to three directories.

--- bash/themes/simple_prompt.bash
+++ bash/themes/simple_prompt.bash
@@ -5,7 +5,8 @@ function parse_git_dirty {
 function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ (\1$(parse_git_dirty))/"
 }
-
-export PS1="\n\[\033[33m\]\t \[\033[34m\]\w\\[\033[00m\]\[\033[90m\]\$(parse_git_branch)\[\033[00m\]\n\[\033[95m\]$ "
+# shorten displayed path
+export PROMPT_DIRTRIM=3
+export PS1="\n\[\033[33m\]\t \\[\033[90m\][\u] \[\033[34m\]\w\\[\033[00m\]\[\033[90m\]\$(parse_git_branch)\[\033[00m\]\n\[\033[95m\]$ "
 #enable __git_ps1
 #export PS1="\n\t \[\033[32m\]\w\[\033[33m\]\$(GIT_PS1_SHOWUNTRACKEDFILES=1 GIT_PS1_SHOWDIRTYSTATE=1 __git_ps1)\[\033[00m\] $ "
