125195a174387133d3019c1454ce1dc3c8706414
Frederic Heath <F.Heath@posteo.net>
2025-06-02 14:33:29 +0200

    Added aliases and environment variables to improve application compliance with XDG Base Directory Specification.

--- env/interactive.sh
+++ env/interactive.sh
@@ -9,17 +9,18 @@ export INPUTRC=${XDG_CONFIG_HOME}/input/config
 export SDIRS=${XDG_DATA_HOME}/sdirs
 export MARKPATH=${XDG_DATA_HOME}/marks
 
-export WGETRC="$XDG_CONFIG_HOME"/wgetrc
+export WGETRC="${XDG_CONFIG_HOME}"/wgetrc
 export MAVEN_OPTS=-Dmaven.repo.local="${XDG_DATA_HOME}/maven/repository"
-export GRADLE_HOME="$XDG_DATA_HOME"/gradle
-export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
-export PATH="$PATH:$XDG_DATA_HOME/cargo/bin"
+export GRADLE_HOME="${XDG_DATA_HOME}"/gradle
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="${XDG_CONFIG_HOME}"/java
+export PATH="$PATH:${XDG_DATA_HOME}/cargo/bin"
 export RUSTUP_HOME="${XDG_DATA_HOME}"/rustup
 export CARGO_HOME="${XDG_DATA_HOME}"/cargo
-export GOPATH=${XDG_DATA_HOME}/go
+export GOPATH="${XDG_DATA_HOME}"/go
+export CCACHE_DIR="${XDG_CONFIG_HOME}"/ccache
 
-[ -z "$NVM_DIR" ] && export NVM_DIR="$XDG_DATA_DIR"/nvm
-export NVM_DIR="$XDG_DATA_HOME"/nvm
+export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}"/npm/npmrc
+export NVM_DIR="${XDG_DATA_HOME}"/nvm
 
 source /usr/share/nvm/nvm.sh
 source /usr/share/nvm/install-nvm-exec
