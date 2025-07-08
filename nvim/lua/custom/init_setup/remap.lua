01385c491c691d50a6648e001b4906622d5904d2
Frederic Heath <F.Heath@posteo.net>
2025-07-08 14:47:14 +0200

    Updated diagnostics commands to conform with new naming/behaviour.

--- nvim/lua/custom/init_setup/remap.lua
+++ nvim/lua/custom/init_setup/remap.lua
@@ -10,8 +10,8 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 --vim.keymap.set('n', 'j', 'v:count == 0 ? "gj" : "kj"', { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '[d', vim.diagnostic.get_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.get_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
