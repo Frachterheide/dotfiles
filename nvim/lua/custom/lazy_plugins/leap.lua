6c847851f50722ede3c1021921a249eb359d8d6a
Frederic Heath <F.Heath@posteo.net>
2026-02-02 13:13:19 +0100

    Updated leap repository to Codeberg location. Updated configuration, removed deprecated create_default_mappings method in favor of Sneak-style vim keybinds.

--- nvim/lua/custom/lazy_plugins/leap.lua
+++ nvim/lua/custom/lazy_plugins/leap.lua
@@ -1,9 +1,12 @@
 return {
-	'ggandor/leap.nvim',
+	url = "https://codeberg.org/andyg/leap.nvim",
 	dependencies = {
 		'tpope/vim-repeat'
 	},
 	config = function()
-		require('leap').create_default_mappings()
+		require('leap')
+		vim.keymap.set({'n', 'x', 'o'}, 's',	'<Plug>(leap-forward)')
+		vim.keymap.set({'n', 'x', 'o'}, 'S',	'<Plug>(leap-backward)')
+		vim.keymap.set('n',				'gs',	'<Plug>(leap-from-window)')
 	end
 }
